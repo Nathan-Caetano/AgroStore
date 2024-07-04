273020509e4de849f30873949afab13f9ca7e04d
Nathan <nathancaetano71@gmail.com>
2024-07-04 14:18:32 -0300

    Adicionando os primeiros produtos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//Menu vars//
 const navbar = document.querySelector('.navbar');
 const menuButtom = document.querySelector('.menu-buttom');
 
+//Menu code//
 menuButtom.addEventListener('click', () => {
     navbar.classList.toggle('show-menu')
 });
