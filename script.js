4aa3bcd97426040453fb4b0988be3c2b0dad3073
Nathan <nathancaetano71@gmail.com>
2024-08-06 12:11:43 -0300

    Atualização do Projeto

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ const menuButtom = document.querySelector('.menu-buttom');
 menuButtom.addEventListener('click', () => {
     navbar.classList.toggle('show-menu')
 });
+
+//comments carousel//
+let currentSlide = 0;
+
+function moveSlide() {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+
+    currentSlide = (currentSlide + 1) % totalSlides;
+
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+setInterval(moveSlide, 3000); 
+
+document.querySelectorAll('a[href^="#"').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
