64b0bd6319d5133e15e685bb81f7c5287fd373c2
Nathan <nathancaetano71@gmail.com>
2024-08-07 17:08:13 -0300

    Animações

--- script.js
+++ script.js
@@ -30,3 +30,51 @@ document.querySelectorAll('a[href^="#"').forEach(anchor => {
         });
     });
 });
+
+//Animações//
+
+let target = null;
+document.addEventListener("DOMContentLoaded",function(){
+    target = document.querySelectorAll('[data-anime]');
+})
+
+const animationClass ='animate'
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+    target.forEach(function(element){
+        if((windowTop)>element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+
+    });
+}
+
+window.addEventListener('scroll', function() {
+    animeScroll();
+})
+
+let targetBlur = null;
+document.addEventListener("DOMContentLoaded",function(){
+    targetBlur = document.querySelectorAll('[data-background]');
+})
+
+const blurClass = 'blur'
+
+function blurScroll() {
+    const windowTop = window.pageYOffset;
+    targetBlur.forEach(function(element){
+        if((windowTop)>element.offsetTop){
+            element.classList.add(blurClass);
+        }else{
+            element.classList.remove(blurClass);
+        }
+
+    });
+}
+
+window.addEventListener('scroll', function() {
+    blurScroll();
+})
